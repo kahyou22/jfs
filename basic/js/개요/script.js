07cd054714aa4e36bb8ef13cddba3cd2bcb74b4f
kahyou22 <kahyou222@gmail.com>
2025-10-28 12:10:48 +0900

    자바스크립트 시작하기

---
+++ basic/js/개요/script.js
@@ -0,0 +1,6 @@
+function fn2() {
+  alert("JavaScript");
+}
+
+var btn2 = document.getElementById("btn2");
+btn2.onclick = fn2;
---
+++ basic/js/개요/script2.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", function () {
+  var btn4 = document.getElementById("btn4");
+  btn4.onclick = function () {
+    alert("자바스크립트 세계에 오신 것을 환영합니다.");
+  };
+});
