1de2d098bd2117e76bf24cf5a9e55e9b9501658e
kahyou22 <kahyou222@gmail.com>
2025-11-04 15:01:42 +0900

    시계

--- js/clock.js
+++ js/clock.js
@@ -23,20 +23,44 @@ function digitalClock() {
   const date = new Date();
   let day;
 
-  if (date.getDay() == 0) {
-    day = "일";
-  } else if (date.getDay() == 1) {
-    day = "월";
-  } else if (date.getDay() == 2) {
-    day = "화";
-  } else if (date.getDay() == 3) {
-    day = "수";
-  } else if (date.getDay() == 4) {
-    day = "목";
-  } else if (date.getDay() == 5) {
-    day = "금";
-  } else if (date.getDay() == 6) {
-    day = "토";
+  // if (date.getDay() == 0) {
+  //   day = "일";
+  // } else if (date.getDay() == 1) {
+  //   day = "월";
+  // } else if (date.getDay() == 2) {
+  //   day = "화";
+  // } else if (date.getDay() == 3) {
+  //   day = "수";
+  // } else if (date.getDay() == 4) {
+  //   day = "목";
+  // } else if (date.getDay() == 5) {
+  //   day = "금";
+  // } else {
+  //   day = "토";
+  // }
+
+  switch (date.getDay()) {
+    case 0:
+      day = "일";
+      break;
+    case 1:
+      day = "월";
+      break;
+    case 2:
+      day = "화";
+      break;
+    case 3:
+      day = "수";
+      break;
+    case 4:
+      day = "목";
+      break;
+    case 5:
+      day = "금";
+      break;
+    default:
+      day = "토";
+      break;
   }
 
   let hour = date.getHours();
