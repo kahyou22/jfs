334a8c5c915d99ea3c7b6d90c1be25f537907340
정문주 <>
2025-12-11 15:09:38 +0900

    개인사이트/메가박스

--- megabox/js/common.js
+++ megabox/js/common.js
@@ -1,9 +1,35 @@
 // header 2depth background
-$(".main li:not(:nth-of-type(5)").mouseenter((e) => {
+// $(".main li:not(:nth-of-type(5)").mouseenter(function () {
+//   $("#header").addClass("active");
+//   $(this).find(".sub").addClass("active");
+// });
+// $(".main > li").mouseleave(() => {
+//   $("#gnb .sub").removeClass("active");
+// });
+// $("#header .subbar").mouseenter(() => {
+//   $(".main > li").find(".sub").addClass("active");
+// });
+// $("#header .subbar").mouseleave(() => {
+//   $("#header").removeClass("active");
+//   $(".main > li").find(".sub").removeClass("active");
+// });
+
+$(".main > li:not(:nth-of-type(5)").mouseenter((e) => {
+  $(".sub").removeClass("active");
+  const target = $(e.currentTarget);
+  target.addClass("active");
+  target.find(".sub").addClass("active");
   $("#header").addClass("active");
 });
-$(".main li").mouseleave(() => {
+
+$(".main > li").mouseleave((e) => {
+  const target = $(e.currentTarget);
+  target.removeClass("active");
+});
+
+$("#header .subbar").mouseleave((e) => {
   $("#header").removeClass("active");
+  $(".sub").removeClass("active");
 });
 
 // footer - looking for theater
